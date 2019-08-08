de76938e1223e8194314f8cc839cb5acbb1dd7a8
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-08-08 16:55:08 +0200

    encoder: add prototype encoder for the compact approach

---
+++ src/encoder/compact_encoder.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "timed_automata.h"
+#include <string>
+#include <vector>
+namespace taptenc {
+class CompactEncoder : Encoder {
+public:
+  void encodeNoOp(AutomataSystem &s, ::std::vector<State> &targets,
+                  const ::std::string opsync, int base_index = 0);
+  void encodeFuture(AutomataSystem &s, ::std::vector<State> &targets,
+                    const ::std::string opsync, const Bounds bounds,
+                    int base_index = 0);
+  void encodePast(AutomataSystem &s, ::std::vector<State> &targets,
+                  const ::std::string opsync, const Bounds bounds,
+                  int base_index = 0);
+};
+} // end namespace taptenc
