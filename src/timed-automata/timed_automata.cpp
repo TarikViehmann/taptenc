1a2d1f3a4d596d00369ef0a6b81035f918a806ad
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-08-08 16:44:45 +0200

    timed-automata: add datatypes to store timed automata models
    The datatypes are oriented at the uppaal functionalities of timed
    automata systems, but are simplified to our expected needs. At the
    moment constraints are just strings, a proper model may be future work.
    Visual information such as node positions are not contained as we
    distinguish between theoretic model and visualization.

---
+++ src/timed-automata/timed_automata.cpp
@@ -0,0 +1,4 @@
+#include "timed_automata.h"
+using namespace taptenc;
+
+int State::obj_count = 0;
---
+++ src/timed-automata/timed_automata.h
@@ -0,0 +1,95 @@
+#pragma once
+#include "../utils.h"
+#include "constraints.h"
+#include <algorithm>
+#include <string>
+#include <vector>
+
+namespace taptenc {
+
+struct state {
+  static int obj_count;
+  ::std::string id;
+  ::std::string name;
+  ::std::string inv;
+  bool urgent;
+  bool initial;
+  state(::std::string arg_name, ::std::string arg_inv, bool arg_urgent = false,
+        bool arg_initial = false) {
+    id = "id" + ::std::to_string(obj_count);
+    obj_count++;
+    name = arg_name;
+    inv = arg_inv;
+    urgent = arg_urgent;
+    initial = arg_initial;
+  }
+};
+typedef struct state State;
+
+struct transition {
+  ::std::string source_id;
+  ::std::string dest_id;
+  ::std::string guard;
+  ::std::string update;
+  ::std::string sync;
+  bool passive;
+  transition(::std::string arg_source_id, ::std::string arg_dest_id,
+             ::std::string arg_guard, ::std::string arg_update,
+             ::std::string arg_sync, bool arg_passive = false) {
+    source_id = arg_source_id;
+    dest_id = arg_dest_id;
+    guard = arg_guard;
+    update = arg_update;
+    sync = arg_sync;
+    passive = arg_passive;
+  }
+};
+typedef struct transition Transition;
+
+enum ChanType { Binary = 1, Broadcast = 0 };
+struct channel {
+  ChanType type;
+  ::std::string name;
+  channel(ChanType arg_type, ::std::string arg_name) {
+    type = arg_type;
+    name = arg_name;
+  }
+};
+typedef struct channel Channel;
+
+struct automataGlobals {
+  ::std::vector<::std::string> clocks;
+  ::std::vector<::std::string> bool_vars;
+  ::std::vector<Channel> channels;
+};
+typedef struct automataGlobals AutomataGlobals;
+
+struct automaton {
+  ::std::vector<State> states;
+  ::std::vector<Transition> transitions;
+  ::std::vector<::std::string> clocks;
+  ::std::vector<::std::string> bool_vars;
+  ::std::string prefix;
+
+  automaton(::std::vector<State> arg_states,
+            ::std::vector<Transition> arg_transitions,
+            ::std::string arg_prefix) {
+    states = arg_states;
+    if (states.end() ==
+        find_if(states.begin(), states.end(),
+                [](const State &s) -> bool { return s.name == "trap"; })) {
+      states.push_back(State("trap", ""));
+    }
+    transitions = arg_transitions;
+    prefix = arg_prefix;
+  }
+};
+typedef struct automaton Automaton;
+
+struct automataSystem {
+  ::std::vector<::std::pair<Automaton, ::std::string>> instances;
+  AutomataGlobals globals;
+};
+typedef struct automataSystem AutomataSystem;
+
+} // end namespace taptenc
