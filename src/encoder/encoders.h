ef5d73caa0429ed0c4660f5a7ab32b24d6622352
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-08-15 14:37:55 +0200

    encoder: make encoding utilities part of superclass
    Functionalities to manipulate Automata, transitions and filters are not
    exclusive to the modular encoder. A future direct encoder would also
    benefit from them.

--- src/encoder/encoders.h
+++ src/encoder/encoders.h
@@ -1,24 +1,5 @@
 #pragma once
 
-#include "timed_automata.h"
-#include <string>
-#include <vector>
-
-namespace taptenc {
-class Encoder {
-public:
-  virtual void encodeNoOp(AutomataSystem &s, ::std::vector<State> &targets,
-                          const ::std::string opsync, int base_index = 0) = 0;
-  virtual void encodeFuture(AutomataSystem &s, ::std::vector<State> &targets,
-                            const ::std::string opsync, const Bounds bounds,
-                            int base_index = 0) = 0;
-  virtual void encodePast(AutomataSystem &s, ::std::vector<State> &targets,
-                          const ::std::string opsync, const Bounds bounds,
-                          int base_index = 0) = 0;
-  // virtual void encodeUntil(AutomataSystem &s, ::std::vector<State> targets,
-  // int base_index = 0) = 0; virtual void encodeSince(AutomataSystem &s,
-  // ::std::vector<State> targets, int base_index = 0) = 0;
-};
-} // end namespace taptenc
 #include "compact_encoder.h"
+#include "encoder.h"
 #include "modular_encoder.h"
--- src/encoder/modular_encoder.h
+++ src/encoder/modular_encoder.h
@@ -6,38 +6,6 @@
 
 namespace taptenc {
 class ModularEncoder : Encoder {
-private:
-  // store the original id of a copied state
-  ::std::unordered_map<::std::string, ::std::string> base_ids;
-
-  ::std::string getBaseId(::std::string id);
-  void addToBaseIds(::std::string state_id, ::std::string base_id);
-  void setBaseIds(const Automaton &s);
-  Automaton filterAutomaton(const Automaton &source,
-                            const ::std::vector<State> &states_filter,
-                            ::std::string filter_prefix,
-                            bool strip_constraints = true);
-  Automaton mergeAutomata(const ::std::vector<Automaton> &automata,
-                          ::std::vector<Transition> &interconnections,
-                          ::std::string prefix);
-  ::std::vector<Transition> createCopyTransitionsBetweenTAs(
-      const Automaton &source, const Automaton &dest,
-      const ::std::vector<State> &filter, ::std::string guard,
-      ::std::string update, ::std::string sync, bool passive = true);
-  ::std::vector<Transition> createSuccessorTransitionsBetweenTAs(
-      const Automaton &base, const Automaton &source, const Automaton &dest,
-      const ::std::vector<State> &filter, ::std::string guard,
-      ::std::string update);
-  ::std::vector<State> updateFilter(const Automaton &ta,
-                                    const ::std::vector<State> &filter);
-  ::std::vector<State> reverseFilter(const Automaton &ta,
-                                     const ::std::vector<State> &filter);
-  void addTrapTransitions(Automaton &ta, ::std::vector<State> &sources,
-                          ::std::string guard, ::std::string update,
-                          ::std::string sync, bool passive = true);
-  void addBaseSyncs(AutomataSystem &s, const int base_pos);
-  void addInvariants(Automaton &ta, const ::std::vector<State> filter,
-                     ::std::string inv);
 
 public:
   void encodeNoOp(AutomataSystem &s, ::std::vector<State> &targets,
