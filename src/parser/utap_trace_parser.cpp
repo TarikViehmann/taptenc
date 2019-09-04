7bf54bee51ad1e7f6a5993aadaf502d68396e6b6
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-09-04 12:33:52 +0200

    rcll_perception: add first benchmark

---
+++ src/parser/utap_trace_parser.cpp
@@ -0,0 +1,259 @@
+#include "utap_trace_parser.h"
+#include "filter.h"
+#include "timed_automata.h"
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <unordered_map>
+
+#include <boost/graph/directed_graph.hpp>
+#include <boost/graph/exterior_property.hpp>
+#include <boost/graph/floyd_warshall_shortest.hpp>
+
+typedef int t_weight;
+
+// define the graph type
+typedef boost::property<boost::edge_weight_t, t_weight> EdgeWeightProperty;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                              boost::no_property, EdgeWeightProperty>
+    Graph;
+
+typedef boost::property_map<Graph, boost::edge_weight_t>::type WeightMap;
+
+// Declare a matrix type and its corresponding property map that
+// will contain the distances between each pair of vertices.
+typedef boost::exterior_vertex_property<Graph, t_weight> DistanceProperty;
+typedef DistanceProperty::matrix_type DistanceMatrix;
+typedef DistanceProperty::matrix_map_type DistanceMatrixMap;
+using std::cout;
+using std::endl;
+using std::iostream;
+using std::pair;
+using std::string;
+using std::unordered_map;
+namespace taptenc {
+namespace UTAPTraceParser {
+// type for weight/distance on each edge
+
+pair<int, int>
+determineGlobTime(unordered_map<pair<string, string>, int> differences) {
+  Graph g;
+  unordered_map<string, int> ids;
+  int x = 0;
+  int t0 = 0;
+  int glob = 0;
+  for (const auto &edge : differences) {
+    auto ins_source = ids.insert(::std::make_pair(edge.first.first, x));
+    if (ins_source.second) {
+      if (edge.first.first.find("globtime") != string::npos) {
+        glob = x;
+      }
+      if (edge.first.first.find("t(0)") != string::npos) {
+        t0 = x;
+      }
+      x++;
+    }
+    auto ins_dest = ids.insert(::std::make_pair(edge.first.second, x));
+    if (ins_dest.second) {
+      if (edge.first.second.find("globtime") != string::npos) {
+        glob = x;
+      }
+      if (edge.first.second.find("t(0)") != string::npos) {
+        t0 = x;
+      }
+      x++;
+    }
+    boost::add_edge(ins_source.first->second, ins_dest.first->second,
+                    edge.second, g);
+  }
+  WeightMap weight_pmap = boost::get(boost::edge_weight, g);
+
+  // set the distance matrix to receive the floyd warshall output
+  DistanceMatrix distances(num_vertices(g));
+  DistanceMatrixMap dm(distances, g);
+
+  // find all pairs shortest paths
+  bool valid = floyd_warshall_all_pairs_shortest_paths(
+      g, dm, boost::weight_map(weight_pmap));
+
+  // check if there no negative cycles
+  if (!valid) {
+    std::cerr << "Error - Negative cycle in matrix" << std::endl;
+    return ::std::make_pair(-1, -1);
+  }
+
+  return ::std::make_pair(-distances[t0][glob], distances[glob][t0]);
+}
+
+void replaceStringInPlace(std::string &subject, const std::string &search,
+                          const std::string &replace) {
+  size_t pos = 0;
+  while ((pos = subject.find(search, pos)) != std::string::npos) {
+    subject.replace(pos, search.length(), replace);
+    pos += replace.length();
+  }
+}
+
+string convertCharsToHTML(std::string str) {
+  replaceStringInPlace(str, "&", "&amp;");
+  replaceStringInPlace(str, "<", "&lt;");
+  replaceStringInPlace(str, ">", "&gt;");
+  return str;
+}
+void parseState(std::string &currentReadLine) {
+  unordered_map<pair<string, string>, int> closed_dbm;
+  size_t eow = currentReadLine.find_first_of(" \t");
+  // skip "State: "
+  currentReadLine = currentReadLine.substr(eow + 1);
+  // skip state name
+  eow = currentReadLine.find_first_of(" ");
+  currentReadLine = currentReadLine.substr(eow + 1);
+  while (currentReadLine != "") {
+    eow = currentReadLine.find_first_of("-");
+    string t_source = currentReadLine.substr(0, eow);
+    // skip source state and "-"
+    currentReadLine = currentReadLine.substr(eow + 1);
+    eow = currentReadLine.find_first_of("<");
+    string t_dest = currentReadLine.substr(0, eow);
+    // skip dest state and "<"
+    currentReadLine = currentReadLine.substr(eow + 1);
+    if (currentReadLine.front() == '=') {
+      // skip "="
+      // TODO: respect bounds when calculating time window
+      currentReadLine.erase(0, 1);
+    }
+    eow = currentReadLine.find_first_of(" \t");
+    int t_weight = stoi(currentReadLine.substr(0, eow));
+    currentReadLine = currentReadLine.substr(eow + 1);
+    auto ins = closed_dbm.insert(
+        ::std::make_pair(::std::make_pair(t_source, t_dest), t_weight));
+    if (!ins.second) {
+      cout << "ERROR: duplicate dbm entry" << endl;
+    }
+  }
+  pair<int, int> curr_time = determineGlobTime(closed_dbm);
+  cout << "total time: " << curr_time.first << " to "
+       << ((curr_time.second == ::std::numeric_limits<int>::max())
+               ? "inf"
+               : ::std::to_string(curr_time.second))
+       << endl;
+}
+
+void parseTransition(std::string &currentReadLine, const Automaton &base_ta,
+                     const Automaton &plan_ta) {
+  size_t eow = currentReadLine.find_first_of(" \t");
+  currentReadLine = currentReadLine.substr(eow + 1);
+  eow = currentReadLine.find_first_of(".");
+  string component = currentReadLine.substr(0, eow);
+  // skip component of source id
+  currentReadLine = currentReadLine.substr(eow + 1);
+  eow = currentReadLine.find_first_of(" \t");
+  string source_id = currentReadLine.substr(0, eow);
+  // skip source id
+  currentReadLine = currentReadLine.substr(eow + 1);
+  eow = currentReadLine.find_first_of(".");
+  // skip delimiter -> and component of dest id
+  currentReadLine = currentReadLine.substr(eow + 1);
+  eow = currentReadLine.find_first_of(" \t");
+  string dest_id = currentReadLine.substr(0, eow);
+  eow = currentReadLine.find_first_of("{");
+  // skip dest id (only labels remaining)
+  currentReadLine = currentReadLine.substr(eow + 1);
+  eow = currentReadLine.find_first_of(";");
+  string guard = currentReadLine.substr(0, eow);
+  // skip guard and "; "
+  currentReadLine = currentReadLine.substr(eow + 2);
+  eow = currentReadLine.find_first_of(";");
+  string sync = currentReadLine.substr(0, eow);
+  // skip sync and "; "
+  currentReadLine = currentReadLine.substr(eow + 2);
+  eow = currentReadLine.find_first_of(";");
+  string update = currentReadLine.substr(0, eow);
+  // remove empty guards
+  guard = (guard == "1") ? "" : convertCharsToHTML(guard);
+  sync = (sync == "0") ? "" : convertCharsToHTML(sync);
+  update = (update == "1") ? "" : convertCharsToHTML(update);
+  // obtain action name
+  string pa_source_id = Filter::getPrefix(source_id, TL_SEP);
+  string pa_dest_id = Filter::getPrefix(dest_id, TL_SEP);
+  if (pa_dest_id == "query") {
+    return;
+  }
+  if (pa_source_id != pa_dest_id) {
+    auto pa_trans =
+        ::std::find_if(plan_ta.transitions.begin(), plan_ta.transitions.end(),
+                       [pa_source_id, pa_dest_id, guard, sync,
+                        update](const Transition &t) bool {
+                         return t.source_id == pa_source_id &&
+                                t.dest_id == pa_dest_id &&
+                                guard.find(t.guard) != string::npos &&
+                                sync.find(t.sync) != string::npos &&
+                                update.find(t.update) != string::npos;
+                       });
+    if (pa_trans == plan_ta.transitions.end()) {
+      cout << "ERROR:  cannot find plan ta transition: " << pa_source_id
+           << " -> " << pa_dest_id << " {" << guard << "; " << sync << "; "
+           << update << "}" << endl;
+    } else if (pa_trans->action != "") {
+      cout << pa_trans->action << endl;
+    } else {
+      cout << "(" << pa_trans->source_id << " -> " << pa_trans->dest_id << ")"
+           << endl;
+    }
+  }
+  string base_source_id = Filter::getSuffix(source_id, BASE_SEP);
+  string base_dest_id = Filter::getSuffix(dest_id, BASE_SEP);
+  if (base_source_id != base_dest_id) {
+    auto base_trans =
+        ::std::find_if(base_ta.transitions.begin(), base_ta.transitions.end(),
+                       [base_source_id, base_dest_id, guard, sync,
+                        update](const Transition &t) bool {
+                         return t.source_id == base_source_id &&
+                                t.dest_id.find(base_dest_id) != string::npos &&
+                                guard.find(t.guard) != string::npos &&
+                                sync.find(t.sync) != string::npos &&
+                                update.find(t.update) != string::npos;
+                       });
+    if (base_trans == base_ta.transitions.end()) {
+      cout << "ERROR:  cannot find base ta transition: " << base_source_id
+           << " -> " << base_dest_id << " {" << guard << "; " << sync << "; "
+           << update << "}" << endl;
+    } else if (base_trans->action != "") {
+      cout << base_trans->action << endl;
+    } else {
+      cout << "(" << base_trans->source_id << " -> " << base_trans->dest_id
+           << ")" << endl;
+    }
+  }
+}
+
+// vector<pair<Transition,size_t>>
+void parseTraceInfo(const std::string &file, const Automaton &base_ta,
+                    const Automaton &plan_ta) {
+
+  // file
+  std::fstream fileStream;
+  std::string currentReadLine;
+  fileStream.open(file, std::fstream::in); // open the file in Input mode
+  cout << "----------------------------------" << endl;
+  cout << "---------Final Plan---------------" << endl;
+  cout << "----------------------------------" << endl;
+  while (getline(fileStream, currentReadLine)) {
+    if (currentReadLine.size() > 5) {
+      if (currentReadLine.substr(0, 5) == "State") {
+        parseState(currentReadLine);
+      }
+      if (currentReadLine.size() > 10 &&
+          currentReadLine.substr(0, 10) == "Transition") {
+        parseTransition(currentReadLine, base_ta, plan_ta);
+      }
+    }
+  }
+  fileStream.close();
+}
+
+} // end namespace UTAPTraceParser
+} // end namespace taptenc
---
+++ src/rcll_perception.cpp
@@ -0,0 +1,365 @@
+#include "encoders.h"
+#include "filter.h"
+#include "printer.h"
+#include "timed_automata.h"
+#include "utap_trace_parser.h"
+#include "vis_info.h"
+#include <algorithm>
+#include <assert.h>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <vector>
+
+using namespace taptenc;
+using namespace std;
+
+void append_prefix_to_states(vector<State> &arg_states, string prefix) {
+  for (auto it = arg_states.begin(); it != arg_states.end(); ++it) {
+    it->name += prefix;
+  }
+}
+Automaton generatePlanAutomaton(const vector<string> &arg_plan,
+                                string arg_name) {
+  vector<string> full_plan;
+  transform(arg_plan.begin(), arg_plan.end(), back_inserter(full_plan),
+            [](const string pa) -> string { return "alpha" + pa; });
+  full_plan.push_back("alphafin");
+  full_plan.insert(full_plan.begin(), "alphastart");
+  vector<State> plan_states;
+  for (auto it = full_plan.begin(); it != full_plan.end(); ++it) {
+    plan_states.push_back(
+        State((*it == "alphafin" || *it == "alphastart")
+                  ? *it
+                  : *it + PA_SEP + to_string(it - full_plan.begin()),
+              (*it == "alphafin" || *it == "alphastart") ? "" : "cpa &lt; 60",
+              false, (*it == "alphastart") ? true : false));
+  }
+  auto find_initial = find_if(plan_states.begin(), plan_states.end(),
+                              [](const State &s) bool { return s.initial; });
+  if (find_initial == plan_states.end()) {
+    cout << "generatePlanAutomaton: no initial state found" << endl;
+  } else {
+    cout << "generatePlanAutomaton: initial state: " << find_initial->id
+         << endl;
+  }
+  vector<Transition> plan_transitions;
+  int i = 0;
+  for (auto it = plan_states.begin() + 1; it < plan_states.end(); ++it) {
+    string sync_op = "";
+    string guard = "";
+    string update = "";
+    auto prev_state = (it - 1);
+    if (prev_state->id != "alphafin" && prev_state->id != "alphastart") {
+      guard = "cpa &gt; 30";
+      update = "cpa = 0";
+    }
+    plan_transitions.push_back(Transition(prev_state->id, it->id, it->id, guard,
+                                          update, sync_op, false));
+    i++;
+  }
+  Automaton res = Automaton(plan_states, plan_transitions, arg_name, false);
+  res.clocks.push_back("cpa");
+  return res;
+}
+
+Automaton generateSyncPlanAutomaton(
+    const vector<string> &arg_plan,
+    const unordered_map<string, vector<string>> &arg_constraint_activations,
+    string arg_name) {
+  vector<pair<int, vector<string>>> syncs_to_add;
+  vector<string> full_plan;
+  transform(arg_plan.begin(), arg_plan.end(), back_inserter(full_plan),
+            [](const string pa) -> string { return "alpha" + pa; });
+  for (auto it = arg_plan.begin(); it != arg_plan.end(); ++it) {
+    auto activate = arg_constraint_activations.find(*it);
+    if (activate != arg_constraint_activations.end()) {
+      syncs_to_add.push_back(
+          make_pair(it - arg_plan.begin(), activate->second));
+    }
+  }
+  int offset = 0;
+  for (auto it = syncs_to_add.begin(); it != syncs_to_add.end(); ++it) {
+    full_plan.insert(full_plan.begin() + offset + it->first, it->second.begin(),
+                     it->second.end());
+    offset += it->second.size();
+  }
+  full_plan.push_back("alphafin");
+  full_plan.insert(full_plan.begin(), "alphastart");
+  vector<State> plan_states;
+  for (auto it = full_plan.begin(); it != full_plan.end(); ++it) {
+    bool urgent = false;
+    if (it->substr(0, 5) != "alpha") {
+      urgent = true;
+    }
+    plan_states.push_back(
+        State(*it + PA_SEP + to_string(it - full_plan.begin()),
+              ((*it == "alphafin" || *it == "alphastart") ? "" : "cpa &lt; 60"),
+              urgent));
+  }
+  vector<Transition> plan_transitions;
+  int i = 0;
+  for (auto it = plan_states.begin() + 1; it < plan_states.end(); ++it) {
+    string sync_op = "";
+    string guard = "";
+    string update = "";
+    auto prev_state = (it - 1);
+    if (prev_state->name.substr(0, 5) != "alpha") {
+      sync_op = Filter::getPrefix(prev_state->name, PA_SEP);
+    } else {
+      guard = "cpa &gt; 30";
+      update = "cpa = 0";
+    }
+    plan_transitions.push_back(Transition(prev_state->id, it->id, it->id, guard,
+                                          update, sync_op, false));
+    i++;
+  }
+  Automaton res = Automaton(plan_states, plan_transitions, arg_name, false);
+  res.clocks.push_back("cpa");
+  return res;
+}
+
+DirectEncoder createDirectEncoding(
+    AutomataSystem &direct_system,
+    const unordered_map<string, vector<string>> &activations,
+    unordered_map<string, pair<vector<State>, Bounds>> &targets,
+    int plan_index = 1) {
+  DirectEncoder enc(direct_system);
+  for (const auto &pa : direct_system.instances[plan_index].first.states) {
+    string pa_op = Filter::getPrefix(pa.id, PA_SEP);
+    if (pa_op.substr(5) == "start" || pa_op.substr(5) == "fin") {
+      continue;
+    }
+    string pa_id = pa_op.substr(5);
+    auto search = activations.find(pa_id);
+    if (search != activations.end()) {
+      for (const auto &ac : search->second) {
+        auto search = targets.find(ac);
+        if (search != targets.end()) {
+          if (ac.substr(0, 6) == "vision") {
+            enc.encodeFuture(direct_system, search->second.first, pa.id,
+                             "vision", search->second.second, 0);
+          }
+          if (ac.substr(0, 7) == "cam_off") {
+            enc.encodeNoOp(direct_system, search->second.first, pa.id);
+          }
+          if (ac.substr(0, 10) == "puck_sense") {
+            enc.encodeFuture(direct_system, search->second.first, pa.id,
+                             "puck_sense", search->second.second, 0);
+          }
+        }
+      }
+    }
+  }
+  return enc;
+}
+
+ModularEncoder
+createModularEncoding(AutomataSystem &system, const AutomataGlobals g,
+                      unordered_map<string, vector<State>> &targets, Bounds b) {
+  ModularEncoder enc(system);
+  for (const auto chan : g.channels) {
+    auto search = targets.find(chan.name);
+    if (search != targets.end()) {
+      if (chan.name.substr(0, 5) == "snoop") {
+        enc.encodeNoOp(system, search->second, chan.name);
+      }
+      if (chan.name.substr(0, 5) == "spast") {
+        enc.encodePast(system, search->second, chan.name, b);
+      }
+      if (chan.name.substr(0, 8) == "sfinally") {
+        enc.encodeFuture(system, search->second, chan.name, b);
+      }
+    }
+  }
+  return enc;
+}
+
+CompactEncoder
+createCompactEncoding(AutomataSystem &system, const AutomataGlobals g,
+                      unordered_map<string, vector<State>> &targets, Bounds b) {
+  CompactEncoder enc;
+  for (const auto chan : g.channels) {
+    auto search = targets.find(chan.name);
+    if (search != targets.end()) {
+      if (chan.name.substr(0, 5) == "snoop") {
+        enc.encodeNoOp(system, search->second, chan.name);
+      }
+      if (chan.name.substr(0, 5) == "spast") {
+        enc.encodePast(system, search->second, chan.name, b);
+      }
+      if (chan.name.substr(0, 8) == "sfinally") {
+        enc.encodeFuture(system, search->second, chan.name, b);
+      }
+    }
+  }
+  return enc;
+}
+
+std::string trim(const std::string &str,
+                 const std::string &whitespace = " \t") {
+  const auto strBegin = str.find_first_not_of(whitespace);
+  if (strBegin == std::string::npos)
+    return ""; // no content
+
+  const auto strEnd = str.find_last_not_of(whitespace);
+  const auto strRange = strEnd - strBegin + 1;
+
+  return str.substr(strBegin, strRange);
+}
+
+void deleteEmptyLines(const std::string &filePath) {
+  std::string bufferString = "";
+
+  // file
+  std::fstream fileStream;
+  std::string currentReadLine;
+  fileStream.open(filePath, std::fstream::in); // open the file in Input mode
+
+  // Read all the lines till the end of the file
+  while (getline(fileStream, currentReadLine)) {
+    // Check if the line is empty
+    if (!currentReadLine.empty()) {
+      currentReadLine = trim(currentReadLine);
+      bufferString = bufferString + currentReadLine + "\n";
+    }
+  }
+  fileStream.close();
+  fileStream.open(filePath,
+                  std::ios::out |
+                      std::ios::trunc); // open file in Output mode. This line
+                                        // will delete all data inside the file.
+  fileStream << bufferString;
+  fileStream.close();
+}
+
+void replaceStringInPlace(std::string &subject, const std::string &search,
+                          const std::string &replace) {
+  size_t pos = 0;
+  while ((pos = subject.find(search, pos)) != std::string::npos) {
+    subject.replace(pos, search.length(), replace);
+    pos += replace.length();
+  }
+}
+
+string convertCharsToHTML(std::string str) {
+  replaceStringInPlace(str, "&", "&amp;");
+  replaceStringInPlace(str, "<", "&lt;");
+  replaceStringInPlace(str, ">", "&gt;");
+  return str;
+}
+
+std::string getEnvVar(std::string const &key) {
+  char *val = std::getenv(key.c_str());
+  if (val == NULL) {
+    cout << "Environment Variable " << key << " not set!" << endl;
+    return "";
+  }
+  return std::string(val);
+}
+
+int main() {
+  vector<State> states;
+  vector<Transition> transitions;
+  vector<string> whatever;
+  states.push_back(State("idle", "", false, true));
+  states.push_back(State("cam_boot", "cam &lt; 5"));
+  states.push_back(State("cam_on", ""));
+  states.push_back(State("save_pic", "pic &lt;= 2"));
+  states.push_back(State("icp_start", "icp &lt;= 10"));
+  states.push_back(State("icp_end", ""));
+  states.push_back(State("puck_sense", "sense &lt; 5"));
+
+  transitions.push_back(
+      Transition("idle", "cam_boot", "power_on_cam", "", "cam = 0", "", true));
+  transitions.push_back(
+      Transition("cam_boot", "cam_on", "", "cam &gt; 2", "cam = 0", "", true));
+  transitions.push_back(
+      Transition("cam_on", "save_pic", "store_pic", "", "pic = 0", "", true));
+  transitions.push_back(
+      Transition("save_pic", "cam_on", "", "pic &gt; 1", "", "", true));
+  transitions.push_back(
+      Transition("cam_on", "icp_start", "start_icp", "", "icp = 0", "", true));
+  transitions.push_back(Transition("icp_start", "icp_end", "",
+                                   "icp &lt; 10 &amp;&amp; icp &gt; 5", "", "",
+                                   true));
+  transitions.push_back(Transition("icp_end", "cam_on", "", "", "", "", true));
+  transitions.push_back(
+      Transition("cam_on", "idle", "power_off_cam", "", "cam = 0", "", true));
+  transitions.push_back(Transition("idle", "puck_sense", "check_puck",
+                                   "cam &gt; 2", "sense = 0", "", true));
+  transitions.push_back(
+      Transition("puck_sense", "idle", "", "sense &gt; 1", "", "", true));
+  Automaton test(states, transitions, "main", false);
+  test.clocks.insert(test.clocks.end(),
+                     {"icp", "cam", "pic", "sense", "globtime"});
+  vector<State> vision_filter;
+  vector<State> cam_off_filter;
+  vector<State> puck_sense_filter;
+  ;
+  vision_filter.insert(vision_filter.end(), {test.states[5]});
+  cam_off_filter.insert(cam_off_filter.end(), {test.states[0], test.states[6]});
+  puck_sense_filter.insert(puck_sense_filter.end(), {test.states[6]});
+  Bounds vision_bounds;
+  vision_bounds.l_op = "&lt;=";
+  vision_bounds.r_op = "&lt;";
+  vision_bounds.x = 0;
+  vision_bounds.y = numeric_limits<int>::max();
+  Bounds cam_off_bounds;
+  Bounds puck_sense_bounds;
+  puck_sense_bounds.l_op = "&lt;=";
+  puck_sense_bounds.r_op = "&lt;";
+  puck_sense_bounds.x = 0;
+  puck_sense_bounds.y = 15;
+  AutomataGlobals glob;
+  XMLPrinter printer;
+  vector<pair<Automaton, string>> automata_direct;
+  automata_direct.push_back(make_pair(test, ""));
+  vector<string> plan{"goto", "pick",
+                      "goto"}; //, "put", "goto", "pick", "discard", "goto",
+                               //"pick", "goto", "put"};
+  unordered_map<string, vector<string>> activations;
+  activations["goto"] = vector<string>{"cam_off"};
+  activations["pick"] = vector<string>{"vision", "puck_sense"};
+  activations["put"] = vector<string>{"vision", ""};
+  activations["discard"] = vector<string>{"cam_off", "puck_sense"};
+  unordered_map<string, pair<vector<State>, Bounds>> targets;
+  targets["cam_off"] = make_pair(cam_off_filter, cam_off_bounds);
+  targets["vision"] = make_pair(vision_filter, vision_bounds);
+  targets["puck_sense"] = make_pair(puck_sense_filter, puck_sense_bounds);
+  Automaton plan_ta = generatePlanAutomaton(plan, "plan");
+  automata_direct.push_back(make_pair(plan_ta, ""));
+  AutomataSystem direct_system;
+  direct_system.instances = automata_direct;
+  DirectEncoder enc1 =
+      createDirectEncoding(direct_system, activations, targets);
+  enc1.createFinalSystem(direct_system);
+  auto internal = enc1.getInternalTAs();
+  SystemVisInfo direct_system_vis_info(internal, plan_ta.states);
+  printer.print(direct_system, direct_system_vis_info, "perception_direct.xml");
+  std::ofstream myfile;
+  myfile.open("perception_direct.q", std::ios_base::trunc);
+  myfile << "E<> sys_direct.query";
+  myfile.close();
+  string call_get_if =
+      "UPPAAL_COMPILE_ONLY=1 " + getEnvVar("VERIFYTA_DIR") +
+      "/verifyta perception_direct.xml - > perception_direct.if";
+  std::system(call_get_if.c_str());
+  string call_get_trace = getEnvVar("VERIFYTA_DIR") +
+                          "/verifyta -t 1 -f perception_direct "
+                          "perception_direct.xml perception_direct.q";
+  std::system(call_get_trace.c_str());
+  deleteEmptyLines("perception_direct-1.xtr");
+  string call_make_trace_readable =
+      "tracer perception_direct.if perception_direct-1.xtr > "
+      "perception_direct.trace";
+  std::system(call_make_trace_readable.c_str());
+  UTAPTraceParser::parseTraceInfo("perception_direct.trace", test, plan_ta);
+  return 0;
+}
