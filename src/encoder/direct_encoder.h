5a24a4aa19b9d6d5f606fac3e1b0a35b39c4bec4
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-09-17 14:05:16 +0200

    direct-encoder: refactor encodeUntilChain
    The tasks necessary to perform an until chain can be grouped into
    different operations on time wondows (vertical slices of the concurrent
    timelines). This greatly improves the code readability and can be used
    to simplify the encoding of other operators as well.

--- src/encoder/direct_encoder.h
+++ src/encoder/direct_encoder.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "encoder.h"
+#include "filter.h"
 #include "timed_automata.h"
 #include "vis_info.h"
 #include <string>
@@ -7,6 +8,7 @@
 #include <vector>
 
 namespace taptenc {
+typedef ::std::unordered_map<::std::string, ::std::string> OrigMap;
 class DirectEncoder : Encoder {
 private:
   size_t encode_counter = 0;
@@ -16,24 +18,42 @@ private:
   ::std::vector<::std::string> pa_order;
   void generateBaseTimeLine(AutomataSystem &s, const int base_pos,
                             const int plan_pos);
+  void addOutgoingTransOfOrigTL(const TimeLine &orig_tl, TimeLine &new_tl,
+                                const OrigMap &to_orig,
+                                ::std::string guard = "");
   ::std::vector<Transition>
-  createTransitionsBackToOrigTL(::std::vector<Transition> &trans,
+  createTransitionsBackToOrigTL(const ::std::vector<Transition> &trans,
                                 ::std::string prefix, ::std::string pa,
                                 ::std::string guard = "");
   ::std::vector<Transition>
   addToPrefixOnTransitions(const ::std::vector<Transition> &trans,
-                           ::std::string prefix, bool only_inner_trans = false);
+                           ::std::string prefix, bool on_inner_trans = true,
+                           bool on_outgoing_trans = true);
   void modifyTransitionsToNextTl(::std::vector<Transition> &trans,
                                  ::std::string curr_pa, ::std::string guard,
-                                 ::std::string update, ::std::string sync);
+                                 ::std::string update, ::std::string sync,
+                                 ::std::string op_name = "");
   void removeTransitionsToNextTl(::std::vector<Transition> &trans,
                                  ::std::string curr_pa);
   ::std::pair<int, int> calculateContext(const EncICInfo &info,
                                          ::std::string starting_pa,
                                          ::std::string ending_pa = "",
                                          int lb_offset = 0, int ub_offset = 0);
+  OrigMap createOrigMapping(const TimeLines &orig_tls, ::std::string prefix);
+  TimeLines createWindow(const TimeLines &origin, ::std::string start_pa,
+                         ::std::string end_pa, const Filter &base_filter,
+                         const Filter &target_filter, ::std::string prefix,
+                         ::std::string clock, const EncICInfo &info);
+  void createTransitionsBetweenWindows(
+      const Automaton &base_ta, TimeLines &source_tls, TimeLines &dest_tls,
+      const ::std::unordered_map<std::string, ::std::string> &map_to_orig,
+      ::std::string start_pa, ::std::string end_pa, ::std::string guard,
+      ::std::string update);
+  void mergeWindows(TimeLines &dest, const TimeLines &to_add,
+                    bool overwrite = false);
 
 public:
+  static void printTLs(const TimeLines &to_print);
   size_t getPlanTAIndex();
   void encodeUntilChain(AutomataSystem &s, const ::std::vector<EncICInfo> &info,
                         const ::std::string start_pa,
