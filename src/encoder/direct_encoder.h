6e71931aed2dff75fab3cc2cab199f36dcf164a3
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-10-18 18:53:30 +0200

    encoder-utils: add doxygen documentation

--- src/encoder/direct_encoder.h
+++ src/encoder/direct_encoder.h
@@ -1,54 +1,348 @@
+/** \file
+ * Encodes temporal constraints by creating copies of the platform TA to
+ * represent different timelines.
+ *
+ * \author: (2019) Tarik Viehmann
+ */
 #pragma once
+#include "../timed-automata/timed_automata.h"
+#include "../timed-automata/vis_info.h"
 #include "enc_interconnection_info.h"
 #include "encoder_utils.h"
 #include "filter.h"
 #include "plan_ordered_tls.h"
-#include "timed_automata.h"
-#include "vis_info.h"
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 namespace taptenc {
 typedef ::std::unordered_map<::std::string, ::std::string> OrigMap;
+/**
+ * Encodes temporal constraints by creating copies of the platform TAs to
+ * represent different timelines.
+ *
+ * The basic idea is to represent a the operation of a platform TA during a plan
+ * by building a cross product between the plan TA and the platform TA.
+ * Constraints may either be encoded by deleting/modifying exisitng states and
+ * transitions (e.g. in case of an invariant during a plan action) or have a
+ * window where the constraint restricts the platform usage and that does not
+ * coincide with an action begin or duration. In this case one can create one
+ * timeline (= copy of the exisiting construction) to represent the time window
+ * in which the constraint may be active and add transitions to it according to
+ * the activation window.
+ *
+ * ~~~
+ * Example schematics of such a construction:
+ * Step 1: Plan with actions P1, P2, P3
+ *         =>
+ *         Plan Automaton: P1->P2->P3
+ * Step 2: Component Automaton M and Plan Automaton
+ *         =>
+ *         M->M->M
+ * Step 3: Add designated query state q
+ *         =>
+ *         M->M->M->q
+ * Step 4: Constraint "be in part M1 of M sometimes after P2 started"
+ *         =>
+ *         M->M ->M       <-- Timeline where the constraint is not satisfied yet
+ *            |   |
+ *            v   v
+ *            M1->M1->q   <-- Timeline where the constraint is satisfied
+ * Result: any a run that reaches q satisfies the
+ *         constraint
+ * ~~~
+ *
+ * The resulting automaton system may consist of many states and clock, but the
+ * reachability query that has to be solved to obtain a valid plan
+ * transformation is simple.
+ */
 class DirectEncoder {
 private:
+  /** The datastructure to represent the encoding.*/
   PlanOrderedTLs po_tls;
+  /**
+   * Plan with action durations.
+   *
+   * Required to calculate the context of constraints (see calculateContext()).
+   */
   ::std::vector<PlanAction> plan;
+  /** Stores the position of the plan TA inside the automata system used to
+   * construct the encoder instance.
+   */
   size_t plan_ta_index;
+  /**
+   * Counts the number of encoded constraints.
+   *
+   * Used to ensure unique clock names to avoid problems when one constraint
+   * fires multiple times during a plan with overlapping active window.
+   */
   size_t encode_counter = 0;
+  /**
+   * Creates a product TA between platform TA and plan TA.
+   *
+   * The result is stored within \a po_tls, so as seperate automata copies.
+   *~~~
+   * Let M be a platform model TA. a1,...,an plan actions.
+   * Plan action  :  a1  a2  a3  ...   an
+   * Base Timeline:  M - M - M - ... - M
+   *
+   * po_tls[ai][ai].ta = M
+   *~~~
+   * @param s Automata system that contains the platform model and the plan
+   * @param base_pos position of the platform model in
+   *        AutomataSystem::instances of \a s
+   * \param plan_pos position of the plan in AutomataSystem::instances of \a s
+   */
   void generateBaseTimeLine(AutomataSystem &s, const int base_pos,
                             const int plan_pos);
+
+  /**
+   * Calculate the window of plan actions during which a temporal constraint is
+   * active.
+   *
+   * @param specs the time constraints to determine the context of
+   * @param starting_pa the plan action which triggers the constraint activation
+   * @param ending_pa If specified, gives a bound of the window.
+   *                  The context ends before the specified ending plan
+   *                  action.
+   * @param look_ahead determines the direction to build the window.
+   *                   true: context starts from \a starting_pa and propagates
+   *                         to the future
+   *                   false: context propagates to the past of \a starting_pa
+   * @param lb_offset initial lower bound of time that is already elapsed from
+   *                  \a starting_pa.
+   * @param ub_offset initial upper bound of time that is already elapsed from
+   *                  \a starting_pa.
+   */
   ::std::pair<int, int> calculateContext(const TargetSpecs &specs,
                                          ::std::string starting_pa,
                                          ::std::string ending_pa = "",
                                          bool look_ahead = true,
                                          int lb_offset = 0, int ub_offset = 0);
+
+  /**
+   * Constructs a direct encoder by copying the the arguments to member
+   * variables.
+   *
+   * Only internally used.
+   */
   DirectEncoder(const PlanOrderedTLs &tls,
                 const ::std::vector<PlanAction> &plan,
                 const size_t plan_ta_index);
 
 public:
   size_t getPlanTAIndex();
+
+  /**
+   * Encode an until chain.
+   *
+   * An until chain is classified by a sequence of target states with
+   * associated temporal constraints:
+   *~~~
+   * Example:
+   * Let M1, M2, M3 be a sequence of target states with bounds [1,2] each
+   * Let P0, P1, P2, P3, P4 be a sequence of plan actions with bounds [1,1] each
+   * Let P1 be the starting_pa and P4 be the ending_pa
+   * Plan    :P0 P1 P2 P3 P4
+   * Encoding: M-M1-M1    M
+   *                |     |
+   *                M2-M2 |
+   *                   | /
+   *                   M3
+   *~~~
+   * The encoding is done within the \a tls member and is not applied to the
+   * automata system.
+   *
+   * @param s automata system containing the platform model
+   * @param info temporal information on the different parts of the chain
+   * @param start_pa plan action where the until chain is activated
+   * @param end_pa plan action that ends the until chain
+   * @param base_pos position of the platform TA in AutomataSystem::instances
+   *        of \a s
+   */
   void encodeUntilChain(AutomataSystem &s, const ChainInfo &info,
                         const ::std::string start_pa,
                         const ::std::string end_pa, const int base_pos = 0);
+
+  /**
+   * Encode an Invariant.
+   * An invariant is classified by a set of target states and a plan action
+   * where it is applied, meaning that during the plan action only the target
+   * states can be visited.
+   *
+   * @param s automata system containing the platform model
+   * @param targets states from the platform model that form the invariant
+   * @param pa plan action name where the invariant should be encoded at
+   */
   void encodeInvariant(AutomataSystem &s, const ::std::vector<State> &targets,
                        const ::std::string pa);
+  /**
+   * Encodes a constraint expressing that upon start of a plan action some
+   * target states should be reached.
+   *
+   * After the states are reached the platform can leave those target state even
+   * while the plan action is still running (which is the difference compared to
+   * the invariant encoding, see encodeInvariant())
+   *
+   * @param s automata system containing the platform model
+   * @param targets states from the platform model
+   * @param pa plan action name upon which start the target state have to be
+   *           reached
+   */
   void encodeNoOp(AutomataSystem &s, const ::std::vector<State> &targets,
                   const ::std::string pa);
+  /**
+   * Encodes a basic constraint stating that in the future some target states
+   * should be met.
+   *
+   *~~~
+   * Example:
+   * Let M1 be the target states with bounds [2,3]
+   * Let P0, P1, P2, P3, P4 be a sequence of plan actions with bounds [1,1] each
+   * Let P1 be the starting_pa
+   * Plan    : P0 P1 P2 P3 P4
+   * Encoding: M--M--M--M  M
+   *                 !  ! /
+   *                 M--M
+   *
+   * where ! denotes transitions to the target states M1 within M
+   *~~~
+   *
+   * @param s automata system containing the platform model
+   * @param pa plan action name upon which start the target state have to be
+   * @param info states from the platform model that should be reached together
+   *        with time bounds
+   * @param base_index index of the platform TA in AutomataSystem::instances of
+   * \a s
+   */
   void encodeFuture(AutomataSystem &s, const ::std::string pa,
                     const UnaryInfo &info, int base_index = 0);
+  /**
+   * Encodes a constraint stating that upon reaching some plan action the
+   * platform should be first in one set of target states and then later,
+   * within some bounds in another set of target states.
+   *
+   *~~~
+   * Example:
+   * Let M1 and M2 be the target states with bounds [2,3]
+   * Let P0, P1, P2, P3, P4 be a sequence of plan actions with bounds [1,1] each
+   * Let P1 be the starting_pa
+   * Plan    : P0 P1 P2 P3 P4
+   * Encoding: M--M1-M1-M1 M
+   *                 !  ! /
+   *                 M--M
+   *
+   * where ! denotes transitions to the target states M2 within M
+   *~~~
+   *
+   * @param s automata system containing the platform model
+   * @param pa plan action name upon which start the constraint fires
+   * @param info states from the platform model that should be reached together
+   *        with time bounds
+   * @param base_index index of the platform TA in AutomataSystem::instances of
+   * \a s
+   */
   void encodeUntil(AutomataSystem &s, const ::std::string pa,
                    const BinaryInfo &info, int base_index = 0);
+
+  /**
+   * Encodes a constraint stating that upon reaching some plan action the
+   * platform should have been in set of target states since
+   * within some bounds it got there from another set of target states.
+   *
+   *~~~
+   * Example:
+   * Let M1 and M2 be the target states with bounds [2,3]
+   * Let P0, P1, P2, P3, P4 be a sequence of plan actions with bounds [1,1] each
+   * Let P4 be the starting_pa
+   * Plan    : P0 P1 P2 P3 P4
+   * Encoding: M--M--M     M
+   *              !  !    /
+   *              M1-M1-M1
+   *
+   * where ! denotes transitions from the target states M2 within M
+   *~~~
+   *
+   * @param s automata system containing the platform model
+   * @param pa plan action name upon which start the constraint fires
+   * @param info states from the platform model that should be reached together
+   *        with time bounds
+   * @param base_index index of the platform TA in AutomataSystem::instances of
+   * \a s
+   */
   void encodeSince(AutomataSystem &s, const ::std::string pa,
                    const BinaryInfo &info, int base_index = 0);
+
+  /**
+   * Encodes a constraint stating that upon reaching some plan action the
+   * platform should have been in set of target states in the past within some
+   * bounds.
+   *
+   *~~~
+   * Example:
+   * Let M1 be the target states with bounds [2,3]
+   * Let P0, P1, P2, P3, P4 be a sequence of plan actions with bounds [1,1] each
+   * Let P4 be the starting_pa
+   * Plan    : P0 P1 P2 P3 P4
+   * Encoding: M--M--M     M
+   *              !  !    /
+   *              M--M--M
+   *
+   * where ! denotes transitions from the target states M1 wiithin M
+   *~~~
+   *
+   * @param s automata system containing the platform model
+   * @param pa plan action name upon which start the constraint fires
+   * @param info states from the platform model that should be reached together
+   *        with time bounds
+   * @param base_index index of the platform TA in AutomataSystem::instances of
+   * \a s
+   */
   void encodePast(AutomataSystem &s, const ::std::string pa,
                   const UnaryInfo &info, int base_index = 0);
+
+  /**
+   * Creates a DirectEncoder instance based of an automata system containing a
+   * platform model and a sequential plan.
+   *
+   * The resulting instance can then be used without further preparation to
+   * encode constraints.
+   *
+   * @param s automata system containing the platform model
+   * @param plan sequential plan, assuming the ordering in the vector
+   *             corresponds to the ordering within the plan
+   * @param base_pos position of the platform model TA in \a s
+   */
   DirectEncoder(AutomataSystem &s, const ::std::vector<PlanAction> &plan,
                 const int base_pos = 0);
+
+  /**
+   * Converts the PlanOrderedTLs representation of the encoding to an automata
+   * systen containing only one automaton and creates visual information for
+   * that system.
+   *
+   * Before converting the representation, iterative pruning is applied to
+   * ignore automata copies without outgoing transitions.
+   *
+   * @param s automata system containing the platform model and plan automaton
+   * @param s_vis instance to store the visual information associated with the
+   *              output automata system
+   * @return automata system containing the automata that contains all encoding
+   *         information
+   */
   AutomataSystem createFinalSystem(const AutomataSystem &s,
                                    SystemVisInfo &s_vis);
+  /**
+   * Create a DirectEncoder Instance containing a merged encoding of this and
+   * the argument encoding.
+   *
+   * Mergin is done by constructing the product automata between encodings.
+   * The resulting encoder should not be used for further encoding steps except
+   * for further merges.
+   *
+   * @param enc2 encoding to merge with
+   * @return encoder containing the merge between \a this and \a enc2
+   */
   DirectEncoder mergeEncodings(const DirectEncoder &enc2) const;
 };
 } // end namespace taptenc
--- src/encoder/encoder_utils.h
+++ src/encoder/encoder_utils.h
@@ -1,36 +1,185 @@
+/** \file
+ * Utility functions to modify automata.
+ *
+ * TODO: Review functions, whether they belong in filter class
+ *
+ * \author: (2019) Tarik Viehmann
+ */
+
 #pragma once
 
-#include "timed_automata.h"
+#include "../timed-automata/timed_automata.h"
 #include <string>
 #include <vector>
 
 namespace taptenc {
+/**
+ * Contains utility functions to modify automatas.
+ */
 namespace encoderutils {
+
+/**
+ * Merges multiple automata together, states with identical names are melted
+ * together.
+ *
+ * @param automata automata to merge together
+ * @param interconnections transitions between the automata
+ * @param prefix prefix of the merged automaton
+ * @return Automaton consisting of all states and transitions from the
+ *         automata in \a automata and the transitions from
+ *         \a interconnections
+ */
 Automaton mergeAutomata(const ::std::vector<Automaton> &automata,
                         ::std::vector<Transition> &interconnections,
                         ::std::string prefix);
+
+/**
+ * Creates transitions from a TA to one of its copies from each state to its
+ * copied state.
+ *
+ * @param source automaton with the source states
+ * @param dest automaton with the dest states
+ * @param filter states that should be connected.
+ *        Other states are ignored when creating transitions.
+ *        Only the state id suffix after constants::BASE_SEP is matched.
+ * @param guard guard on the created transitions
+ * @param update update on the created transitions
+ * @param sync sync channel on the created transitions
+ * @param passive is the sync emitting or receiving?
+ */
 ::std::vector<Transition>
 createCopyTransitionsBetweenTAs(const Automaton &source, const Automaton &dest,
                                 const ::std::vector<State> &filter,
                                 ::std::string guard, ::std::string update,
                                 ::std::string sync, bool passive = true);
+
+/**
+ * Creates transitions from a TA to one of its copies by adding transitions from
+ * the base automaton with a source id contained in the TA and dest id contained
+ * in the target copy.
+ *
+ * @param base automaton with all transitions that should be considered.
+ *        Typically this is the automaton where \a source and \a dest were based
+ *        of.
+ * @param source automaton with the source states
+ * @param dest automaton with the dest states
+ * @param filter states that should be connected.
+ *        Other states are ignored when creating transitions.
+ *        Only the state id suffix after constants::BASE_SEP is matched.
+ * @param guard guard on the created transitions
+ * @param update update on the created transitions
+ */
 ::std::vector<Transition> createSuccessorTransitionsBetweenTAs(
     const Automaton &base, const Automaton &source, const Automaton &dest,
     const ::std::vector<State> &filter, ::std::string guard,
     ::std::string update);
+
+/**
+ * Adds transitions to the trap state.
+ *
+ * @param ta automaton with trap state
+ * @param sources states form \a ta that should be connected to the trap
+ * @param guard guard on the created transitions
+ * @param update update on the created transitions
+ * @param sync sync channel on the created transitions
+ * @param passive is the sync emitting or receiving?
+ */
 void addTrapTransitions(Automaton &ta, const ::std::vector<State> &sources,
                         ::std::string guard, ::std::string update,
                         ::std::string sync, bool passive = true);
+
+/**
+ * Adds sync channels on each transition of an automaton.
+ *
+ * Allows to have a base automaton in a system which syncronizes all transitions
+ * with any copy that is made from it.
+ *
+ * @param s automata system containing the base automaton
+ * @param base_pos positio of hte automaton in \a s
+ */
 void addBaseSyncs(AutomataSystem &s, const int base_pos);
+
+/**
+ * Adds invariants to the specified states of a TA.
+ *
+ * @param ta automaton to add invariants to
+ * @param filter states that should receive the invariant.
+ *               A state from \a filter matches one from \a ta, if it is
+ *               a prefix form the \a ta state.
+ * @param inv invariant to add
+ */
 void addInvariants(Automaton &ta, const ::std::vector<State> filter,
                    ::std::string inv);
+/**
+ * Creates a formated prefix with constants::TL_SEP, constants::CONSTRAINT_SEP
+ * and constants::BASE_SEP.
+ *
+ * @param op operator (between TL_SEP and CONSTRAINT_SEP)
+ * @param sub next operator (between CONSTRAINT_SEP and BASE_SEP)
+ * @param pa plan action (before TL_SEP)
+ * @return \a pa + TL_SEP + \a op + CONSTRAINT_SEP + \a sub + BASE_SEP
+ */
 ::std::string toPrefix(::std::string op, ::std::string sub = "",
                        ::std::string pa = "");
+
+/**
+ * Adds an operator to an existing prefix by adding it directly after
+ * constants::TL_SEP.
+ *
+ * @param prefix a string containing constants::TL_SEP,
+ * constants::CONSTRAINT_SEP and constants::BASE_SEP
+ * @param op operator, gets placed between TL_SEP and the first CONSTRAINT_SEP
+ *           of \a prefix
+ * @param sub next operator, gets placed after \a op separated by CONSTRAINT_SEP
+ * @return pa (from prefix) + TL_SEP + \a op + CONSTRAINT_SEP + \a sub + rest of
+ *  \a prefix
+ */
 ::std::string addToPrefix(::std::string prefix, ::std::string op,
                           ::std::string sub = "");
+/**
+ * Adds a base id to an existing id.
+ *
+ * This becomes necessary when building the product automaton of two TAs.
+ * Typically not called directly but rather via mergeIds().
+ *
+ * @param id full id of a state
+ * @param to_add base id of another sate (suffix after constants::BASE_SEP)
+ * @return \a id + constants::COMPONENT_SEP + \a to_add
+ */
 ::std::string addToBaseId(::std::string id, ::std::string to_add);
+
+/**
+ * Combines the action labels of two transitions.
+ *
+ * This becomes necessary when building the product automaton of two TAs.
+ * The ordering of the parameters plays a role and should correspond to the
+ * ordering specified when merging state ids together via mergeIds().
+ *
+ * @param action1 action label of one transition
+ * @param action2 action label of another transition
+ * @return \a action1 + constants::ACTION_SEP + \a action2
+ */
 ::std::string mergeActions(::std::string action1, ::std::string action2);
+/**
+ * Merges two state ids together.
+ *
+ * @param id1 id to be merged into
+ * @param id2 id to merge
+ * @return id1 with id2's base id appended and id2's prefix (between
+ *         constants::TL_SEP and constants::BASE_SEP) added to it's own prefix
+ */
 ::std::string mergeIds(::std::string id1, ::std::string id2);
+
+/**
+ * Construct a plan automaton from a sequential plan.
+ *
+ * The plan automaton further encodes start and end actions.
+ * They are required to express constraints that scope before the first real
+ * plan action or after the last one.
+ *
+ * @param plan sequential plan to encode as automaton
+ * @param name automaton name for the resulting TA
+ */
 Automaton generatePlanAutomaton(const ::std::vector<PlanAction> &plan,
                                 ::std::string name);
 } // namespace encoderutils
