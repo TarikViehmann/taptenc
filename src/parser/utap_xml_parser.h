fc7d54879dc12b942f42e0610451290dd55e4125
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-10-21 12:54:13 +0200

    utap-xml-parser: add Doxygen documentation

--- src/parser/utap_xml_parser.h
+++ src/parser/utap_xml_parser.h
@@ -1,7 +1,17 @@
-#include "timed_automata.h"
+/** \file
+ * XML parser utilizing the utap lib parser
+ *
+ * \author (2019) Tarik Viehmann
+ */
+#include "../timed-automata/timed_automata.h"
 #include <string>
 namespace taptenc {
 namespace utapxmlparser {
+/**
+ * Read an XML system.
+ * @param filename name of xml file
+ * @return AutomataSystem containing the info from the xml system.
+ */
 AutomataSystem readXMLSystem(::std::string filename);
 } // end namespace utapxmlparser
 } // end namespace taptenc
