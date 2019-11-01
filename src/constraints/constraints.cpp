f7562ed48c00225f5d4481a93cd1ab13fd972c89
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2019-11-01 14:42:33 +0100

    constraints: start refactoring clock constraints WIP
    Represent clock constraints by proper objects instead of raw strings.
    This allows for more straight forward interfacing with other programs
    and gives better control over constraint manipulation.
    
    Only clock constraints from very basic timed automata syntax is
    supported, in particular:
     - conjunctions, basic comparisons (clock against a constant) and
    difference comparisons (clock difference against a constant)
    are supported
     - constants in comparisons are always on the right hand side
     - no negations
     - no disjunctions (as they are also not supported by uppaal)
     - no variables (control booleans or numeric variables) are supported)

--- src/constraints/constraints.cpp
+++ src/constraints/constraints.cpp
@@ -1,29 +1,170 @@
+/** \file
+ * Datastructures to represent clock constraints.
+ *
+ * \author (2019) Tarik Viehmann
+ */
+
 #include "constraints.h"
+#include <cassert>
 #include <iostream>
+#include <limits>
+#include <memory>
+
 using namespace taptenc;
 
-std::string taptenc::reverse_op(std::string op) {
-  if (op == "&lt;")
-    return "&gt;";
-  if (op == "&gt;")
-    return "&lt;";
-  if (op == "&lt;=")
-    return "&gt;=";
-  if (op == "&gt;=")
-    return "&lt;=";
-  std::cout << "reverse_op: unknown op:" << op << std::endl;
-  return "reverse_op: error";
+clock::clock(::std::string arg_id) : id(arg_id) {}
+
+// Comparison Utils
+
+::std::string computils::toString(const ComparisonOp comp) {
+  switch (comp) {
+  case LTE:
+    return " &lt;= ";
+    break;
+  case LT:
+    return " &lt; ";
+    break;
+  case GT:
+    return " &gt; ";
+    break;
+  case GTE:
+    return " &gt;= ";
+    break;
+  case EQ:
+    return " == ";
+    break;
+  case NEQ:
+    return " != ";
+    break;
+  default:
+    return "unknown_op";
+    break;
+  }
 }
 
-std::string taptenc::inverse_op(std::string op) {
-  if (op == "&lt;")
-    return "&gt;=";
-  if (op == "&gt;")
-    return "&lt;=";
-  if (op == "&lt;=")
-    return "&gt;";
-  if (op == "&gt;=")
-    return "&lt;";
-  std::cout << "inverse_op: unknown op:" << op << std::endl;
-  return "inverse_op: error";
+ComparisonOp computils::reverseOp(ComparisonOp op) {
+  if (op == ComparisonOp::LT)
+    return ComparisonOp::GT;
+  if (op == ComparisonOp::GT)
+    return ComparisonOp::LT;
+  if (op == ComparisonOp::LTE)
+    return ComparisonOp::GTE;
+  if (op == ComparisonOp::GTE)
+    return ComparisonOp::LTE;
+  if (op == ComparisonOp::EQ)
+    return ComparisonOp::EQ;
+  if (op == ComparisonOp::NEQ)
+    return ComparisonOp::NEQ;
+  std::cout << "reverseOp: unknown op:" << toString(op) << std::endl;
+  return op;
 }
+
+ComparisonOp computils::inverseOp(ComparisonOp op) {
+  if (op == ComparisonOp::LT)
+    return ComparisonOp::GTE;
+  if (op == ComparisonOp::GT)
+    return ComparisonOp::LTE;
+  if (op == ComparisonOp::LTE)
+    return ComparisonOp::GT;
+  if (op == ComparisonOp::GTE)
+    return ComparisonOp::LT;
+  if (op == ComparisonOp::EQ)
+    return ComparisonOp::NEQ;
+  if (op == ComparisonOp::NEQ)
+    return ComparisonOp::EQ;
+  std::cout << "inverseOp: unknown op:" << toString(op) << std::endl;
+  return op;
+}
+
+// Constraints
+
+// TrueCC
+trueCC::trueCC() { type = CCType::TRUE; }
+
+::std::string trueCC::toString() const { return "1"; }
+
+::std::unique_ptr<ClockConstraint> trueCC::createCopy() const {
+  return std::make_unique<TrueCC>(TrueCC());
+}
+
+// ConjunctionCC
+conjunctionCC::conjunctionCC(const ClockConstraint &first,
+                             const ClockConstraint &second) {
+  ::std::unique_ptr<ClockConstraint> first_copy = first.createCopy();
+  ::std::unique_ptr<ClockConstraint> second_copy = second.createCopy();
+  content.first = std::move(first_copy);
+  content.second = std::move(second_copy);
+  type = CCType::CONJUNCTION;
+}
+
+::std::string conjunctionCC::toString() const {
+  return content.first->toString() + " &amp;&amp; " +
+         content.second->toString();
+}
+
+::std::unique_ptr<ClockConstraint> conjunctionCC::createCopy() const {
+  return std::make_unique<ConjunctionCC>(
+      ConjunctionCC(*content.first, *content.second));
+}
+
+// ComparisonCC
+comparisonCC::comparisonCC(::std::shared_ptr<Clock> arg_clock,
+                           ComparisonOp arg_comp, timepoint arg_constant)
+    : clock(arg_clock), comp(arg_comp), constant(arg_constant) {
+  type = CCType::SIMPLE_BOUND;
+}
+
+::std::unique_ptr<ClockConstraint> comparisonCC::createCopy() const {
+  ComparisonCC res(clock, comp, constant);
+  return std::make_unique<ComparisonCC>(res);
+}
+
+::std::string comparisonCC::toString() const {
+  return clock->id + computils::toString(comp) + std::to_string(constant);
+}
+
+// DifferenceCC
+differenceCC::differenceCC(::std::shared_ptr<Clock> arg_minuend,
+                           ::std::shared_ptr<Clock> arg_subtrahend,
+                           ComparisonOp arg_comp, timepoint arg_difference)
+    : minuend(arg_minuend), subtrahend(arg_subtrahend), comp(arg_comp),
+      difference(arg_difference) {
+  type = CCType::DIFFERENCE;
+}
+
+::std::unique_ptr<ClockConstraint> DifferenceCC::createCopy() const {
+  return std::make_unique<DifferenceCC>(
+      DifferenceCC(minuend, subtrahend, comp, difference));
+}
+
+::std::string DifferenceCC::toString() const {
+  return minuend.get()->id + " - " + subtrahend.get()->id +
+         computils::toString(comp) + std::to_string(difference);
+}
+
+// Bounds
+
+bounds::bounds(timepoint l, timepoint u) {
+  l_op = ComparisonOp::LTE;
+  r_op = u == ::std::numeric_limits<timepoint>::max() ? ComparisonOp::LT
+                                                      : ComparisonOp::LTE;
+  lower_bound = l;
+  upper_bound = u;
+}
+
+bounds::bounds(timepoint l, timepoint u, ComparisonOp arg_l_op,
+               ComparisonOp arg_r_op) {
+  assert(arg_l_op == ComparisonOp::LT || arg_l_op == ComparisonOp::LTE);
+  assert(arg_r_op == ComparisonOp::LT || arg_r_op == ComparisonOp::LTE);
+  assert(arg_r_op == ComparisonOp::LT ||
+         upper_bound != std::numeric_limits<timepoint>::max());
+  l_op = arg_l_op;
+  r_op = arg_r_op;
+  lower_bound = l;
+  upper_bound = u;
+}
+
+// Plan Action
+planAction::planAction(::std::string arg_name, const Bounds &arg_duration)
+    : name(arg_name), duration(arg_duration),
+      delay_tolerance(0, std::numeric_limits<timepoint>::max()) {}
--- src/constraints/constraints.h
+++ src/constraints/constraints.h
@@ -1,37 +1,263 @@
+/** \file
+ * Datastructures to represent clock constraints.
+ *
+ * \author (2019) Tarik Viehmann
+ */
+
 #pragma once
+
 #include <limits>
+#include <memory>
 #include <string>
 
 namespace taptenc {
 
+/**
+ * Datatype of timepoints. Requires std::numeric_limits< >::max() to be
+ * defined.
+ */
+using timepoint = int;
+
+/**
+ * Clock representation.
+ */
+struct clock {
+  std::string id;
+  clock(::std::string arg_id);
+};
+typedef clock Clock;
+
+enum ComparisonOp {
+  /** <= or &lt;= in html encoding */
+  LTE,
+  /** < or &lt; in html encoding */
+  LT,
+  /** > or &gt; in html encoding */
+  GT,
+  /** >= or &gt;= in html encoding */
+  GTE,
+  /** == */
+  EQ,
+  /** != */
+  NEQ
+};
+namespace computils {
+/**
+ * Returns the html encoding of the a comparison operator with leading and
+ * trailing whitespace.
+ *
+ * @param op comparison operator to get htm encoded string frm
+ * @return html encoding of \a op with leading and trailing whitespace
+ */
+::std::string toString(const ComparisonOp op);
+
+/**
+ * Reverses a comparison operator (reading from right to left instead left to
+ * right).
+ *
+ * E.g. <= becomes >=.
+ *
+ * @param op comparison operator to reverse
+ * @return reversed comparison operator of \a op
+ */
+ComparisonOp reverseOp(ComparisonOp op);
+
+/**
+ * Inverts a comparison operator (negating it).
+ *
+ * E.g. <= becomes >.
+ *
+ * @param op comparison operator to invert
+ * @return inverted comparison operator of \a op
+ */
+ComparisonOp inverseOp(ComparisonOp op);
+} // end namespace computils
+
+/**
+ * Clock Constraint types.
+ */
+enum CCType {
+  /** Conjunction of exactly two clock constraints. */
+  CONJUNCTION,
+  /** Difference constraint (comparison of the clock difference of two clocks
+   * against a constant.
+   */
+  DIFFERENCE,
+  /**
+   * Comparison of a clock against a constant.
+   */
+  SIMPLE_BOUND,
+  /** Trivial constraint, that is always true. */
+  TRUE
+};
+
+/**
+ * Clock constraint base class. Represents the basic clock constraints of
+ * timed automata syntax, but not the extended expressiveness of uppaal.
+ */
+struct clockConstraint {
+  /** Holds the real type concrete object type. */
+  CCType type;
+  /** Copies interface to allow clock constraint copies extended by more
+   * constraints during encoding.
+   */
+  virtual ::std::unique_ptr<struct clockConstraint> createCopy() const = 0;
+  /**
+   * Converts constraints into the proper format for xml files.
+   *
+   * Operators &, <, > are replaced by their respective html encodings.
+   */
+  virtual ::std::string toString() const = 0;
+};
+typedef struct clockConstraint ClockConstraint;
+
+/** True constraint. */
+struct trueCC : public ClockConstraint {
+  ::std::unique_ptr<struct clockConstraint> createCopy() const;
+  ::std::string toString() const;
+  trueCC();
+};
+typedef struct trueCC TrueCC;
+
+/**
+ * Conjunction of exactly two clock constraints. Nesting is possible to
+ * express arbitrary long conjunctions.
+ */
+struct conjunctionCC : public ClockConstraint {
+  /** Subexpressions that form the conjunction. */
+  std::pair<std::unique_ptr<clockConstraint>, std::unique_ptr<clockConstraint>>
+      content;
+
+  ::std::unique_ptr<ClockConstraint> createCopy() const;
+  ::std::string toString() const;
+
+  /**
+   * Creates a new conjunction given two clock constraints.
+   *
+   * Copies the input clock constraints to form a completely independent
+   * constraint.
+   *
+   * @param first lhs of the conjunction
+   * @param second rhs of the conjunction
+   */
+  conjunctionCC(const ClockConstraint &first, const ClockConstraint &second);
+};
+typedef struct conjunctionCC ConjunctionCC;
+
+/**
+ * Comparison of a clock against a constant. The constant is always on the rhs
+ * of the comparison operator.
+ */
+struct comparisonCC : public ClockConstraint {
+  ::std::shared_ptr<Clock> clock;
+  ComparisonOp comp;
+  timepoint constant;
+  /**
+   * Creates a comparison of a clock on the left side against a constant on
+   * the right side.
+   *
+   * @param arg_clock clock that forms the lhs of the comparison
+   * @param arg_comp comparison operator
+   * @param constant constant that forms the rhs of the comparison
+   */
+  comparisonCC(::std::shared_ptr<Clock> arg_clock, ComparisonOp arg_comp,
+               timepoint constant);
+
+  ::std::unique_ptr<ClockConstraint> createCopy() const;
+  ::std::string toString() const;
+};
+typedef struct comparisonCC ComparisonCC;
+
+/**
+ * Comparison of a clock difference against a constant. The constant is always
+ * on the rhs of the comparison operator.
+ */
+struct differenceCC : public ClockConstraint {
+  ::std::shared_ptr<Clock> minuend;
+  ::std::shared_ptr<Clock> subtrahend;
+  ComparisonOp comp;
+  timepoint difference;
+  /**
+   * Creates a comparison of a clock difference on the left side against a
+   * constant on the right side.
+   *
+   * @param arg_minuend clock that forms the lhs of - operator on the lhs of
+   *                    the comparison
+   * @param arg_subtrahend clock that forms the rhs of - operator on the lhs of
+   *                       the comparison
+   * @param arg_comp comparison operator
+   * @param arg_difference constant that forms the rhs of the comparison
+   */
+  differenceCC(::std::shared_ptr<Clock> arg_minuend,
+               ::std::shared_ptr<Clock> arg_subtrahend, ComparisonOp arg_comp,
+               timepoint arg_difference);
+
+  ::std::unique_ptr<ClockConstraint> createCopy() const;
+  ::std::string toString() const;
+};
+typedef struct differenceCC DifferenceCC;
+
+/**
+ * Wrapper to store lower and upper bounds.
+ *
+ * Read as lower bound {<,<=} content {<,<=} upper bound.
+ * The content it self is not part of this datatype.
+ */
 struct bounds {
-  ::std::string l_op;
-  ::std::string r_op;
-  int lower_bound;
-  int upper_bound;
-  bounds(int l, int u) {
-    l_op = "&lt;=";
-    r_op = u == ::std::numeric_limits<int>::max() ? "&lt;" : "&lt;=";
-    lower_bound = l;
-    upper_bound = u;
-  }
-  bounds(int l, int u, ::std::string arg_l_op, ::std::string arg_r_op) {
-    l_op = arg_l_op;
-    r_op = arg_r_op;
-    lower_bound = l;
-    upper_bound = u;
-  }
+  ComparisonOp l_op;
+  ComparisonOp r_op;
+  timepoint lower_bound;
+  timepoint upper_bound;
+  /**
+   * Creates non-strct bounds.
+   *
+   * @param l lower bound
+   * @param u upper bound
+   */
+  bounds(timepoint l, timepoint u);
+
+  /**
+   * Creates bounds with user defined comparison operatos.
+   *
+   * Only valid bounds can be created. Bounds are invalid if their operators are
+   * neither ComparisonOp::LT nor ComparisonOp::LTE or if the upper bound is
+   * infinity but the upper bound operator is non-strict.
+   *
+   * @param l lower bound
+   * @param u upper bound
+   * @param arg_l_op lower bound operator (ComparisonOp::LT or ComparisonOp::LTE
+   *                 expected)
+   * @param arg_r_op upper bound operator (ComparisonOp::LT or ComparisonOp::LTE
+   *                 expected)
+   */
+  bounds(timepoint l, timepoint u, ComparisonOp arg_l_op,
+         ComparisonOp arg_r_op);
 };
 typedef struct bounds Bounds;
 
+/**
+ * Plan action.
+ */
 struct planAction {
   ::std::string name;
   Bounds duration;
-  planAction(::std::string arg_name, const Bounds &arg_duration)
-      : name(arg_name), duration(arg_duration) {}
+  /** execution time that the plan transformation determines. */
+  timepoint execution_time;
+  /**
+   * The amount of tolerable delay when executing the selected action.
+   */
+  Bounds delay_tolerance;
+  /**
+   * Creates a plan action.
+   *
+   * Inits execution time to 0 and delay tolerance to be
+   * unbounded [0,infinity).
+   *
+   * @param arg_name name of the plan action
+   * @param arg_duration action duration
+   */
+  planAction(::std::string arg_name, const Bounds &arg_duration);
 };
 typedef struct planAction PlanAction;
 
-::std::string reverse_op(::std::string op);
-::std::string inverse_op(::std::string op);
 } // namespace taptenc
